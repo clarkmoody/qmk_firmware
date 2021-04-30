cdf8f023d61fb96a068944ab549098cf0b1a8b3c
Clark Moody <clarkmoody@gmail.com>
2021-04-29 21:03:29 -0500

    Replace bottom-row Shift with another Cmd

--- keyboards/preonic/keymaps/clarkmoody/keymap.c
+++ keyboards/preonic/keymaps/clarkmoody/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |      | GUI  |Lower |    Space    |Raise | Shift| MOUSE| Menu |  Fn  |
+ * | Ctrl |  Alt |      | GUI  |Lower |    Space    |Raise | GUI  | MOUSE| Menu |  Fn  |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_preonic_1x2uC(
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
   KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, MO(_LWR),      KC_SPC,     MO(_RSE),KC_RSFT, TD(X_MS),KC_APP,  TT(_LGT)
+  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, MO(_LWR),      KC_SPC,     MO(_RSE),KC_RGUI, TD(X_MS),KC_APP,  TT(_LGT)
 ),
 
 /* Qwerty
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |      | GUI  |Lower |    Space    |Raise | Shift| MOUSE| Menu |  Fn  |
+ * | Ctrl |  Alt |      | GUI  |Lower |    Space    |Raise | GUI  | MOUSE| Menu |  Fn  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_1x2uC(
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, MO(_LWR),      KC_SPC,     MO(_RSE),KC_RSFT, TD(X_MS),KC_APP,  TT(_LGT)
+  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, MO(_LWR),      KC_SPC,     MO(_RSE),KC_RGUI, TD(X_MS),KC_APP,  TT(_LGT)
 ),
 
 /* Lower (left thumb)
