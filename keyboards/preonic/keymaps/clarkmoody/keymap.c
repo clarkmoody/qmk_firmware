7f7ae76e9d45da1cabbed91499aa98d5fb200688
Clark Moody <clark@clarkmoody.com>
2022-01-07 00:08:22 -0600

    Dedicated arrow keys, momentary raise & lower
    Escape key in corner. Dedicated DEL key on right side.

--- keyboards/preonic/keymaps/clarkmoody/keymap.c
+++ keyboards/preonic/keymaps/clarkmoody/keymap.c
@@ -17,6 +17,8 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+#define W_SWP G(KC_GRV)
+
 enum preonic_layers {
   _COLEMAK,
   _QWERTY,
@@ -67,131 +69,131 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  |  \   |
+ * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |  "   |
+ * |  \|  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |      | GUI  |>LOWER|    Space    |>RAISE| GUI  |>MOUSE|  Alt | Ctrl |
+ * | Ctrl |  Alt | GUI  |      |>LOWER|    Space    |>RAISE| Left | Down |  Up  | Rght |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_preonic_1x2uC(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
+  KC_BSLS, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, TO(_LWR),      KC_SPC,     TO(_RSE),KC_RGUI, TO(_MSE),KC_RALT, KC_RCTL
+  KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, MO(_LWR),      KC_SPC,     MO(_RSE),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  \   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |  \|  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |      | GUI  |>LOWER|    Space    |>RAISE| GUI  |>MOUSE|  Alt | Ctrl |
+ * | Ctrl |  Alt | GUI  |      |>LOWER|    Space    |>RAISE| Left | Down |  Up  | Rght |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_1x2uC(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  KC_BSLS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  KC_LCTL, KC_LALT, XXXXXXX, KC_LGUI, TO(_LWR),      KC_SPC,     TO(_RSE),KC_RGUI, TO(_MSE),KC_RALT, KC_RCTL
+  KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, MO(_LWR),      KC_SPC,     MO(_RSE),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower (left thumb)
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |      |
+ * |      |   ^  |   &  |   *  |   (  |   )  |      |   _  |   +  |   {  |   }  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |>BASE |      |      |      |      |      |      | Left | Down |  Up  | Rght |      |
+ * | CAPS |   !  |   @  |   #  |   $  |   %  |      |   -  |   =  |   [  |   ]  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |      |      |      |      |      | Shift|
+ * | Shift|      |      |      |      |      |      |   `  |   ~  |   (  |   )  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |>BASE | GUI  |      |             |>ADJ  | GUI  |>MOUSE|  Alt | Ctrl |
+ * | Ctrl |  Alt | GUI  |      |      |             |>ADJ  | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LWR] = LAYOUT_preonic_1x2uC(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
-  TG(_LWR),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
-  _______, _______, TG(_LWR),_______, XXXXXXX,       XXXXXXX,    TO(_ADJ),_______, TO(_MSE),_______, _______
+  XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+  XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+  KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, _______,
+  _______, _______, _______, _______, XXXXXXX,      XXXXXXX,     MO(_ADJ),KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise (right thumb)
  * ,-----------------------------------------------------------------------------------.
- * |  `~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | Del  |
+ * | WSWP | F11  | F12  | F13  | F14  | F15  | F16  | F17  | F18  | F19  | F20  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   _  |   +  |   {  |   }  |      |      |      |      |      |      |      |
+ * |      |   _  |   +  |   {  |   }  |      |   !  |   @  |   #  |   $  |   %  | Del  |
  * |------+------+------+------+------+------+------+------+------+-------------+------|
- * |>BASE |   -  |   =  |   [  |   ]  |      |      |      |      |      |      |      |
+ * | CAPS |   -  |   =  |   [  |   ]  |      |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------+------+------+------+------|------+------|
- * | Shift|      |      |   (  |   )  |      |      |      |      |      |      | Shift|
+ * | Shift|   `  |   ~  |   (  |   )  |      |      |      |      |   |  |   \  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |>BASE | GUI  |>ADJ  |             |      | GUI  |>MOUSE|  Alt | Ctrl |
+ * | Ctrl |  Alt | GUI  |      |>ADJ  |             |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RSE] = LAYOUT_preonic_1x2uC(
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-  XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  TG(_RSE),KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
-  _______, _______, TG(_RSE),_______, TO(_ADJ),      XXXXXXX,    XXXXXXX, _______, TO(_MSE),_______, _______
+  W_SWP,   KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______,
+  XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,
+  KC_CAPS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+  _______, KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, KC_BSLS, _______,
+  _______, _______, _______, _______, MO(_ADJ),     XXXXXXX,     XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Adjust & Media (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |Reset | F11  | F12  | F13  | F14  | F15  | F16  | F17  | F18  | F19  | F20  |Debug |
+ * |Reset |      |      |      |      |      |      |      |      |      |      |Debug |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Prev | Next |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |>BASE |      |      |Qwerty|Colemk|      | Audio| Play | Vol- | Vol+ | Mute |      |
+ * |      |      |      |Qwerty|Colemk|AGnorm| Audio| Play | Vol- | Vol+ | Mute |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |      |      |      |      |      | Shift|
+ * |      |      |      |      |      |AGswap|      |      |      |      |      | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |>BASE | GUI  |      |             |      | GUI  |>MOUSE|  Alt |>LIGHT|
+ * |      |      |      |      |      |   >MOUSE    |      |      |      |      |>LIGHT|
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJ] = LAYOUT_preonic_1x2uC(
-  RESET,   KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  DEBUG,
+  RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DEBUG,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX,
-  TG(_ADJ),XXXXXXX, XXXXXXX, QWERTY,  COLEMAK, XXXXXXX, AU_TOG,  KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
-  _______, _______, TG(_ADJ),_______, XXXXXXX,     XXXXXXX,      XXXXXXX, _______, TO(_MSE),_______, TO(_LGT)
+  XXXXXXX, XXXXXXX, XXXXXXX, QWERTY,  COLEMAK, AG_NORM, AU_TOG,  KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, AG_SWAP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      TO(_MSE),    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(_LGT)
 ),
 
 /* Mouse
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |>BASE |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | W LF | W DN | W UP | W RT |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |>BASE |      |Acc 0 |Acc 1 |Acc 2 |      |      | M LF | M DN | M UP | M RT |      |
+ * |      |      |Acc 0 |Acc 1 |Acc 2 |      |      | M LF | M DN | M UP | M RT |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|      |      |      |      |      |      | BTN1 | BTN2 |      |      | Shift|
+ * | Shift|      |      |      |      |      |      | BTN1 | BTN2 | BTN3 | BTN4 |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  Alt |>BASE | GUI  |      |   Mouse 1   |      | GUI  |      |  Alt | Ctrl |
+ * | Ctrl |  Alt | GUI  |      |>LOWER|   Mouse 1   |>RAISE|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_MSE] = LAYOUT_preonic_1x2uC(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  TG(_MSE),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
-  TG(_MSE),XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, KC_RSFT,
-  _______, _______, TG(_MSE),_______, XXXXXXX,     KC_BTN1,      XXXXXXX, _______, XXXXXXX, _______, _______
+  XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, XXXXXXX,
+  KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, TG(_MSE),     KC_BTN1,     TG(_MSE),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Lighting
  * ,-----------------------------------------------------------------------------------.
- * |      | RGB1 | RGB2 | RGB3 | RGB4 | RGB5 | RGB6 | RGB7 | RGB8 | RGB9 |      |      |
+ * |>BASE | RGB1 | RGB2 | RGB3 | RGB4 | RGB5 | RGB6 | RGB7 | RGB8 | RGB9 |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | <RGB>|Toggle|      |      |Toggle|<Back>|      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -199,15 +201,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      | Eff- | Bri- | Sat- | Hue- |      |      | BL-  | Brth |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |>BASE |      |      |             |      |      |>MOUSE|      |      |
+ * |      |      |      |      |>LOWER|             |>RAISE|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LGT] = LAYOUT_preonic_1x2uC(
-  XXXXXXX, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, XXXXXXX, XXXXXXX,
+  TG(_LGT),RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX, BL_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, XXXXXXX, XXXXXXX, BL_INC,  BL_STEP, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, XXXXXXX, XXXXXXX, BL_DEC,  BL_BRTG, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, TG(_LGT),XXXXXXX, XXXXXXX,     XXXXXXX,      XXXXXXX, XXXXXXX, TO(_MSE),XXXXXXX, XXXXXXX
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_LGT),     XXXXXXX,     TG(_LGT),XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
 
 };
